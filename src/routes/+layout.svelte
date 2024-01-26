c87c5caaaa3cc7c3b67cb9b6dce58c7f797f7eff
Thomas Reed-Munoz <thomas@reedmunoz.com>
2024-01-26 13:19:34 -0500

    theme switcher and about page animation and transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,10 @@
     import '../lib/colors.css';
     import '../lib/fonts.css';
     import { page } from '$app/stores';
+    let dark = false;
 </script>
 
-<div class="wrapper light">
+<div class="wrapper" class:light={!dark} class:dark={dark}>
     <div class="page-header">
         <div class="page-header__content">
             <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 128 128" fill="none">
@@ -49,7 +50,19 @@
     </div>
     
     <footer>
-        <input type="checkbox" class="theme-switch" />
+        <input type="checkbox" id="theme-switch" bind:checked={dark}/>
+        <label for="theme-switch" class="switch">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                <mask id="path-1-inside-1_51_3153" fill="white">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M8.85867 4.64209C9.30785 4.75222 9.74918 4.92703 10.1699 5.16991C12.5613 6.55063 13.3807 9.60858 12 12C10.6193 14.3915 7.56133 15.2109 5.16987 13.8302C4.74918 13.5873 4.37713 13.2925 4.05716 12.9586C4.35008 15.3704 5.73393 17.6199 8 18.9282C11.8263 21.1374 16.7191 19.8264 18.9282 16C21.1373 12.1737 19.8263 7.28098 16 5.07184C13.7339 3.76352 11.0938 3.68984 8.85867 4.64209Z"/>
+                </mask>
+                <path d="M8.85867 4.64209L8.38238 6.58455L2.55199 5.15495L8.07479 2.8021L8.85867 4.64209ZM4.05716 12.9586L2.07174 13.1997L1.34797 7.24038L5.50123 11.5748L4.05716 12.9586ZM9.16987 6.90197C8.9146 6.75459 8.64982 6.65012 8.38238 6.58455L9.33495 2.69962C9.96589 2.85433 10.5838 3.09947 11.1699 3.43786L9.16987 6.90197ZM10.2679 11C11.0964 9.56516 10.6047 7.73039 9.16987 6.90197L11.1699 3.43786C14.5179 5.37086 15.665 9.65199 13.732 13L10.2679 11ZM6.16987 12.0981C7.60475 12.9265 9.43952 12.4349 10.2679 11L13.732 13C11.7991 16.3481 7.51792 17.4952 4.16987 15.5622L6.16987 12.0981ZM5.50123 11.5748C5.69174 11.7737 5.9146 11.9507 6.16987 12.0981L4.16987 15.5622C3.58375 15.2238 3.06252 14.8113 2.61308 14.3423L5.50123 11.5748ZM6.04257 12.7174C6.26294 14.5319 7.30156 16.2156 9 17.1962L7 20.6603C4.1663 19.0243 2.43722 16.2089 2.07174 13.1997L6.04257 12.7174ZM9 17.1962C11.8698 18.853 15.5393 17.8698 17.1961 15L20.6603 17C17.8988 21.783 11.7829 23.4217 7 20.6603L9 17.1962ZM17.1961 15C18.853 12.1303 17.8698 8.46074 15 6.80389L17 3.33979C21.7829 6.10121 23.4217 12.2171 20.6603 17L17.1961 15ZM15 6.80389C13.3016 5.8233 11.3241 5.76568 9.64255 6.48207L8.07479 2.8021C10.8636 1.61401 14.1663 1.70375 17 3.33979L15 6.80389Z" mask="url(#path-1-inside-1_51_3153)"/>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11 5C11 4.44772 11.4477 4 12 4C12.5523 4 13 4.44772 13 5C13 5.55228 12.5523 6 12 6C11.4477 6 11 5.55228 11 5ZM6.34315 7.75736C5.95262 7.36684 5.95262 6.73367 6.34315 6.34315C6.73367 5.95262 7.36683 5.95262 7.75736 6.34315C8.14788 6.73367 8.14788 7.36684 7.75736 7.75736C7.36683 8.14788 6.73367 8.14788 6.34315 7.75736ZM16.2426 7.75736C16.6332 8.14788 17.2663 8.14788 17.6569 7.75736C18.0474 7.36683 18.0474 6.73367 17.6569 6.34315C17.2663 5.95262 16.6332 5.95262 16.2426 6.34315C15.8521 6.73367 15.8521 7.36684 16.2426 7.75736ZM12.0623 17C14.8238 17 17.0623 14.7614 17.0623 12C17.0623 9.23858 14.8238 7 12.0623 7C9.30092 7 7.06235 9.23858 7.06235 12C7.06235 14.7614 9.30092 17 12.0623 17ZM7.75736 17.6569C7.36684 18.0474 6.73367 18.0474 6.34315 17.6569C5.95262 17.2663 5.95262 16.6332 6.34315 16.2426C6.73367 15.8521 7.36684 15.8521 7.75736 16.2426C8.14788 16.6332 8.14788 17.2663 7.75736 17.6569ZM16.2426 16.2426C15.8521 16.6332 15.8521 17.2663 16.2426 17.6569C16.6332 18.0474 17.2663 18.0474 17.6569 17.6569C18.0474 17.2663 18.0474 16.6332 17.6569 16.2426C17.2663 15.8521 16.6332 15.8521 16.2426 16.2426ZM18 12C18 12.5523 18.4477 13 19 13C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11C18.4477 11 18 11.4477 18 12ZM5 13C4.44772 13 4 12.5523 4 12C4 11.4477 4.44772 11 5 11C5.55228 11 6 11.4477 6 12C6 12.5523 5.55228 13 5 13ZM12 18C11.4477 18 11 18.4477 11 19C11 19.5523 11.4477 20 12 20C12.5523 20 13 19.5523 13 19C13 18.4477 12.5523 18 12 18ZM12.0623 15C13.7192 15 15.0623 13.6569 15.0623 12C15.0623 10.3431 13.7192 9 12.0623 9C10.4055 9 9.06234 10.3431 9.06234 12C9.06234 13.6569 10.4055 15 12.0623 15Z" />
+            </svg>
+        </label>
+        <div class="copy">© Thomas Reed-Muñoz 2024</div>
     </footer>
 </div>
 
@@ -63,6 +76,18 @@
         flex-direction: column;
         align-items: flex-start;
         flex-shrink: 0;
+        background-image: url('/Map-Light.svg');
+        background-position: center;
+        background-size: cover;
+        transition: background-color 200ms, background-image 200ms, color 200ms;
+
+        &.light {
+            background-image: url('/Map-Light.svg');
+        }
+
+        &.dark {
+            background-image: url('/Map-Dark.svg');
+        }
     }
 
     .page-header {
@@ -93,7 +118,7 @@
                 text-decoration: none;
                 color: var(--Primary-Color);
                 border: 3px solid transparent;
-                transition: all .2s;
+                transition: all 200ms;
 
                 &:hover {
                     border: 3px solid var(--Border);
@@ -135,6 +160,48 @@
         align-items: center;
         gap: 20px;
         align-self: stretch;
+
+        & .copy {
+            color: var(--Sub-Text);
+            font-family: "Norms Medium";
+            font-size: 14px;
+            letter-spacing: -0.42px;
+        }
+    }
+
+    #theme-switch {
+        display: none;
+
+        &:checked + .switch:after {
+            left: 30px;
+        }
+    }
+
+    .switch {
+        position: relative;
+        display: inline-flex;
+        padding: 3px;
+        align-items: flex-start;
+        align-content: flex-start;
+        gap: 0px 3px;
+        flex-wrap: wrap;
+        border-radius: 999px;
+        background: var(--Primary-Color);
+        color: var(--Background-Primary);
+        cursor: pointer;
+        transition: background-color 200ms, color 200ms;
+
+        &::after {
+            content: '';
+            position: absolute;
+            width: 24px;
+            height: 24px;
+            border-radius: 50%;
+            background-color: var(--Background-Primary);
+            top: 3px;
+            left: 3px;
+            transition: all 200ms;
+        }
     }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,37 @@
-<div class="content__about">About</div>
+<script>
+    import { onMount } from 'svelte';
+    import { fade, fly, scale } from 'svelte/transition';
+	import { expoOut } from 'svelte/easing';
+
+    let ready = false;
+
+    onMount(() => ready = true);
+</script>
+
+{#if ready}
+    <div class="page-about">
+        <div class="about__top">
+            <div class="pin-contain">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="95" viewBox="0 0 64 95" class="pin" transition:fly={{ delay: 500, duration: 1000, y: -150, opacity: 0, easing: expoOut }}>
+                    <path d="M26.5567 85.3958C17.4229 74.405 -3.8147e-06 54.8743 0 32C3.8147e-06 14.3269 14.3269 0 32 0C49.6731 0 64 14.3269 64 32C64 54.8743 46.5771 74.405 37.4433 85.3958C36.0005 87.132 34.0002 88 32 88C29.9997 88 27.9995 87.132 26.5567 85.3958Z" />
+                    <circle cx="32" cy="32" r="16" fill="white"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="12" viewBox="0 0 64 12" class="shadow" transition:scale={{ duration: 1000, delay: 500, opacity: 0, start: 0, easing: expoOut }}>
+                    <ellipse cx="32" cy="6" rx="18" ry="6" />
+                </svg>
+            </div>
+        </div>
+        <div class="about__bottom">
+            <div class="about__header" in:fade={{delay: 200, duration: 200}}>
+                <h1>Thomas Reed-Muñoz</h1>
+                <p>Product and Visual Design in NYC</p>
+            </div>
+        </div>
+    </div>
+{/if}
 
 <style>
-    .content__about {
+    .page-about {
         display: flex;
         padding: 0px 50px;
         flex-direction: column;
@@ -10,5 +40,66 @@
         gap: 30px;
         flex: 1 0 0;
         align-self: stretch;
+    }
+
+    .about__top {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: center;
+        gap: 10px;
+        flex: 1 0 0;
+        align-self: stretch;
+    }
+
+    .pin-contain {
+        display: flex;
+        padding: 12px 0px;
+        gap: 6px;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: center;
+
+        & .pin {
+            fill: var(--Primary-Color);
+            transition: fill 200ms;
+        }
+
+        & .shadow {
+            fill: var(--Shadow);
+            transition: fill 200ms;
+        }
+    }
+
+    .about__bottom {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        flex: 1 0 0;
+        align-self: stretch;
+
+        & .about__header {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 15px;
+            align-self: stretch;
+
+            & h1 {
+                color: var(--Header-Text);
+                font-family: "Norms Bold";
+                font-size: 70px;
+                line-height: 50px;
+                letter-spacing: -0.04em;
+                transition: color 200ms;
+            }
+
+            & p {
+                color: var(--Sub-Text);
+                font-size: 25px;
+                letter-spacing: -0.02em;
+                transition: color 200ms;
+            }
+        }
     }
 </style>
