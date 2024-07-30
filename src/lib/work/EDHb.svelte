ba9ee346f907ccd594526adf30662a44e49d06f5
Thomas Reed-Munoz <thomas@Thoms-MacBook-Pro.local>
2024-07-29 21:56:25 -0400

    edhb work page content

--- src/lib/work/EDHb.svelte
+++ src/lib/work/EDHb.svelte
@@ -7,6 +7,35 @@
     <h2><span>Elder Dragon Handbook</span></h2>
 </div>
 
+<p>EDH (or Commander) is a variant of Magic: The Gathering where players choose a legendary creature card (their commander) to build a deck around. There are many resources for finding and viewing the cards that are able to be a commander, but none of them make it as simple as I wanted. EDHbook was not intended to be better than these other resources, which have many more features and options, but it was intended to be easier. I designed the app in Figma and built it with Sveltekit as a fully functioning&nbsp;proof-of-concept.</p>
+
+<p>Elder Dragon Handbook uses the Scryfall API for card&nbsp;data.</p>
+
+<div class="flex">
+    <a href="http://www.edhbook.com" target="_blank" aria-label="Go to EDHb" class="button">
+        View the app
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 4.5H2.5V13.5H11.5V9" stroke="currentColor" />
+            <path d="M7 9L13.5 2.5M13.5 2.5V9M13.5 2.5H7" stroke="currentColor" />
+        </svg>
+    </a>
+</div>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/EDHb/Image-01_1x.webp, /work/EDHb/Image-01_2x.webp 2x" src="/work/EDHb/Image-01_1x.webp" alt="EDHb Main View" />
+    <span class="small">Main view with card catalog sorting and filtering.</span>
+</div>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/EDHb/Image-02_1x.webp, /work/EDHb/Image-02_2x.webp 2x" src="/work/EDHb/Image-02_1x.webp" alt="EDHb Art View" />
+    <span class="small">View alternate card art.</span>
+</div>
+
+<div class="work-image">
+    <img class="image-portrait" srcset="/work/EDHb/Image-03_1x.webp, /work/EDHb/Image-03_2x.webp 2x" src="/work/EDHb/Image-03_1x.webp" alt="EDHb Mobile View" />
+    <span class="small">Mobile view.</span>
+</div>
+
 <style>
     /* Styles */
 </style>
