2a3c5d1c7e6ac8ae826de3d3664d390d8626fb4e
Thomas Reed-Munoz <thomas@Thoms-MacBook-Pro.local>
2024-07-29 21:07:05 -0400

    nycvectors work page

--- src/lib/work/NYCVectors.svelte
+++ src/lib/work/NYCVectors.svelte
@@ -7,6 +7,18 @@
     <h2><span>NYC Vectors</span></h2>
 </div>
 
+<p>Two vector illustrations for a previous, now unused&nbsp;project.</p>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/NYCVectors/Image-01_1x.webp, /work/NYCVectors/Image-01_2x.webp 2x" src="/work/NYCVectors/Image-01_1x.webp" alt="Subway Token Vector" />
+    <span class="small">Recreation of subway token.</span>
+</div>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/NYCVectors/Image-02_1x.webp, /work/NYCVectors/Image-02_2x.webp 2x" src="/work/NYCVectors/Image-02_1x.webp" alt="Manhattan Map Outline Vector" />
+    <span class="small">Outline map of Manhattan.</span>
+</div>
+
 <style>
     /* Styles */
 </style>
