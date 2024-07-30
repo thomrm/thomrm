e92196b88706bd7c155603865dfcd1a09d2e333f
Thomas Reed-Munoz <thomas@Thoms-MacBook-Pro.local>
2024-07-29 21:40:09 -0400

    twitch reporting page content

--- src/lib/work/TwitchReporting.svelte
+++ src/lib/work/TwitchReporting.svelte
@@ -7,6 +7,52 @@
     <h2><span>Reporting on Twitch</span></h2>
 </div>
 
+<p>The previous iteration of the Twitch report flow was overwhelming. A large number of users who started the report process did not finish it. The flow exposed all available options up front, and there was little consideration for where the report originated. The flow for reporting a live stream and the flow for reporting a chat message started the same way. Along with incomplete reports, this also contributed to many misclassified reports. It placed too much of the burden on the users, and they were not guided to accurate responses. This added more roadblocks for specialists trying to resolve reports, and degraded user trust in the reporting&nbsp;system.</p>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/TwitchReporting/Image-01_1x.webp, /work/TwitchReporting/Image-01_2x.webp 2x" src="/work/TwitchReporting/Image-01_1x.webp" alt="Reporting Step 1" />
+    <span class="small">Reporting Step 1.</span>
+</div>
+
+<p>Breaking the report flow into several progressive steps made for a less intimidating experience for users. First asking what is being reported, then asking why, and finally asking for more context. Starting the flow from an advanced step when applicable also reduces the number of decisions required. Entering the report flow from a live stream would start with 'Live Stream' already selected as the first&nbsp;step.</p>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/TwitchReporting/Image-02_1x.webp, /work/TwitchReporting/Image-02_2x.webp 2x" src="/work/TwitchReporting/Image-02_1x.webp" alt="Reporting Step 2" />
+    <span class="small">Reporting Step 2.</span>
+</div>
+
+<p>For greater report accuracy, we would group relevant reasons with their content types. Presenting smaller and more focused lists of reasons to users to choose from reduces cognitive load. It then becomes easier for users to find and select the most accurate&nbsp;reason.</p>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/TwitchReporting/Image-03_1x.webp, /work/TwitchReporting/Image-03_2x.webp 2x" src="/work/TwitchReporting/Image-03_1x.webp" alt="Reporting Step 3" />
+    <span class="small">Reporting Step 3.</span>
+</div>
+
+<p>The changes we made to the report flow were overwhelmingly successful. We saw a huge increase in completion rate, and our specialists reported more accurate entries. The framework we laid also allowed for rapid iteration, and provided a blueprint for adding new report types and&nbsp;reasons.</p>
+
+<div class="work-image">
+    <img class="image-horizon" srcset="/work/TwitchReporting/Image-04_1x.webp, /work/TwitchReporting/Image-04_2x.webp 2x" src="/work/TwitchReporting/Image-04_1x.webp" alt="Reporting Step 4" />
+    <span class="small">Reporting Step 4.</span>
+</div>
+
+<div class="work-iframe">
+    <div class="iframe-header small">
+        <span>iframe</span>
+        <a href="https://www.figma.com/proto/1t2iYrnDkG56f4mRoxHMJL?kind=proto&node-id=13-2175&page-id=5:224&scaling=scale-down&starting-point-node-id=13:2175&t=bi1FwgeqSb1ImIDe-1&type=design&viewport=203,215,0.49" target="_blank">
+            <span>https://www.figma.com/proto/[...]</span>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 4.5H2.5V13.5H11.5V9" stroke="currentColor" />
+                <path d="M7 9L13.5 2.5M13.5 2.5V9M13.5 2.5H7" stroke="currentColor" />
+            </svg>
+        </a>
+    </div>
+    <iframe 
+        title="Twitch Reporting Prototype"
+        src="https://www.figma.com/embed?embed_host=share&amp;url=https%3A%2F%2Fwww.figma.com%2Fproto%2F1t2iYrnDkG56f4mRoxHMJL%2FTwitch-Reporting%3Fpage-id%3D5%253A224%26type%3Ddesign%26node-id%3D13-2175%26viewport%3D203%252C215%252C0.49%26t%3Dbi1FwgeqSb1ImIDe-1%26scaling%3Dscale-down%26starting-point-node-id%3D13%253A2175%26mode%3Ddesign%26hide-ui%3D1"
+        allowfullscreen="">
+    </iframe>
+</div>
+
 <style>
     /* Styles */
 </style>
